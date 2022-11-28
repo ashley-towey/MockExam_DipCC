26dda2d5d149556ccd94235210ee4bcbd9306666
Ashley Towey <a.towey0220201@arts.ac.uk>
2022-11-28 14:12:25 +0000

    change aspect ratio 600 x 800

--- script.js
+++ script.js
@@ -4,9 +4,14 @@
 // TRUE weather values
 let temp = 0;    // temperature [in degrees]
 let weather = "";   // weathercode [as a string]
-let weatherText = "";
+let weatherText = ""; // weathercode text [as a string]
+let rain = 0; // rain [in millimetres]
+let apparentTemp = 0; // apparent temperature [in degrees]
 
-let json;
+// time variables to count through the array of 168 total values 
+let currentTime;
+let totalTimes;
+let jsonTimeDate;
 
 // starting/base colours
 let bgCol = '#D3D3D3';
@@ -17,6 +22,7 @@ let textCol = '#000000';
 let wmoVals = [];
 let a; // used to count through wmoVals
 let falseTemp = 0; // initialise the wrong temperature
+let falseApparentTemp = 0; // intitialise the apparent false temperature
 
 // x and y for animated circles/rain/snow
 let x = 30;
@@ -30,7 +36,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(600, 800);
 
   // choose a random WMO value from the wmoVals array
   wmoVals = [63, 3, 80, 61, 65, 0, 1, 2, 45, 48, 51, 53, 55, 81, 82, 95, 96, 99, 85, 86, 77, 71, 73, 75]; // an array of possible weathercode values to randomly pick from
@@ -38,18 +44,26 @@ function setup() {
 
   // random FALSE temperature value rounded to 1 decimal place
   falseTemp = round(random(-10, 40), 1);
+  falseApparentTemp = falseTemp + round(random(-3, 3));
 
   // console information to check eveything is working
   console.log("Apparent Weather Predictor");
-  let jsonTimeDate = json.current_weather.time; // get the time and data value
-  let dateTime = jsonTimeDate.split("T"); // split the date and time by "T"
-  console.log(dateTime[0]); // check the date
-  console.log(dateTime[1]); // check the time
+  // let jsonTimeDate = json.current_weather.time; // get the time and data value
+  // let dateTime = jsonTimeDate.split("T"); // split the date and time by "T"
+  // console.log(dateTime[0]); // check the date
+  // console.log(dateTime[1]); // check the time
   console.log("Latitude "+json.latitude); // check latitude
   console.log("Longitude "+json.longitude); // check longitude
   console.log(json.current_weather.temperature+"°"); // check the temperature
   console.log("WMO "+json.current_weather.weathercode); // check the weathercode
   console.log("False WMO "+wmoVals[a]); // check the wrong WMO
+
+  // checking what the time and date are to check against what number in the array
+  // console.log("date/time: "+jsonTimeDate);
+  // let totalTimes = json.hourly.time;
+  // let currentTime = totalTimes.indexOf(jsonTimeDate);
+  // console.log("Number of current time in array: " + currentTime);
+  // console.log("check against time/date above: "+totalTimes[currentTime]);
 
   // mode initialisations
   noStroke();
@@ -69,50 +83,67 @@ function draw() {
     // animation with circles jittering like snow
     for(let i = 0; i < width; i++) {
       fill(255);
-      ellipse(i, y+random(0, 1200), cirSize);
+      ellipse(i, y+random(0, height*2), cirSize);
     }
   }
+  // Write the title sequence
+  fill(textCol);
+  text("Apparent Weather \nForecast", 50, 80);
 }
 
 function trueWeather() {
+  jsonTimeDate = json.current_weather.time; // get the time and data value
+  totalTimes = json.hourly.time;
+  currentTime = totalTimes.indexOf(jsonTimeDate);
+  console.log(currentTime);
+
   temp = json.current_weather.temperature; // get the temperature
   weather = json.current_weather.weathercode; // get weathercode
+  rain = json.hourly.precipitation; // get rain
+  apparentTemp = json.hourly.apparent_temperature; // get the apparent temperature
 
   convertWMO(); // convert WMO codes into readable text format
 
   // Display all the data we got from https://open-meteo.com/en
   background(bgCol); 
   fill(textCol); // text colour fill
-  text(weatherText, 75, 525);
+  text(weatherText, 75, 525+150);
   let g = map(temp, 0, 40, 255, 0);
   let b = map(temp, 0, 40, 255, 0);
   fill(255, g, b); // false weather temp fill
-  text(temp + "°", 450, 250);  
+  text(temp + "°", 450, 400);  
+  text(apparentTemp[currentTime]+"°", 450, 320+150);
   fill(cirCol); // circle fill
-  ellipse(40, 50, 375); // circle affected by weathercode data
-  textSize(12);
-  text("* true weather (ish)", 420, 570);
+  ellipse(40, 200, 375); // circle affected by weathercode data
+  textSize(15);
+  text("* true weather (ish)", 300, 150);
+  // check if there is any rain before adding millimetres
+  if(rain[currentTime] > 0.0){
+    textSize(15);
+    text(rain[currentTime] + "mm", 100, 560+150); // write the rain
+  }
   textSize(50);
 }
 
 function falseWeather() {
     // randomised temp and weathercode values
     weather = wmoVals[a];
-    // weather = 96; // USE to TEST weather values and colours
+    // weather = 85; // USE to TEST weather values and colours
 
     convertWMO(); // convert WMO codes into readable text format
 
     background(bgCol);
     fill(textCol); // text colour fill
-    text(weatherText, 75, 525);
+    text(weatherText, 75, 525+150);
     let falseG = map(falseTemp, 0, 40, 255, 0);
     let falseB = map(falseTemp, 0, 40, 255, 0);
     fill(255, falseG, falseB); // false weather temp fill
-    text(falseTemp + "°", 450, 250);  
+    text(falseTemp + "°", 450, 400);  
+    text(falseApparentTemp + "°", 450, 320+150);
     fill(cirCol);
-    ellipse(40, 50, 375);
-    textSize(12);
-    text("* false weather", 420, 570);
+    ellipse(40, 200, 375);
+    textSize(15);
+    text("* false weather", 300, 150);
     textSize(50);
 }
 
